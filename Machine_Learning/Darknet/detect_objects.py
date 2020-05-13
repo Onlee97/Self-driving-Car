6afbf815a9da805db51857bb4554c86f2c896325
Jiana Koshy <jiana@Jianas-MBP.fios-router.home>
2020-05-12 20:43:22 -0400

    Makeover

--- Machine_Learning/Darknet/detect_objects.py
+++
@@ -1,7 +0,0 @@
-import darknet as d
-
-net = d.load_net("yolov3-tiny.cfg".encode(), "yolov3-tiny.weights".encode(), 0)
-meta = d.load_meta("voc.data".encode())
-r = d.detect(net, meta, "crossingguard.jpg".encode())
-
-print(r)
